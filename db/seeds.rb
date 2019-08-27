b6ea675fc1a563306dbe075d8bf6528a4b6e111c
dani <dani.zaghian@gmail.com>
2019-08-27 11:49:14 -0700

    adds seeds file data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+contact = Contact.new(
+  first_name: "Eddie", 
+  last_name: "Lee", 
+  email: "eddie@gmail.com", 
+  phone_number: "3333333"
+)
+contact.save
+
+contact = Contact.new(
+  first_name: "Todd", 
+  last_name: "Bisel", 
+  email: "todd@gmail.com", 
+  phone_number: "4444444"
+)
+contact.save
+
+contact = Contact.new(
+  first_name: "Jackie", 
+  last_name: "Kartchner", 
+  email: "jackie@gmail.com", 
+  phone_number: "5555555"
+)
+contact.save
