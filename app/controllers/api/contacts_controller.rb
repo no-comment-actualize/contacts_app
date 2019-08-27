6942e150c6e8f4661ac61d0b3350c693ceb9b5d6
dani <dani.zaghian@gmail.com>
2019-08-27 11:26:52 -0700

    adds single contact and array of contacts routes

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,13 @@
+class Api::ContactsController < ApplicationController
+
+  def one_contact_method
+    @contact = Contact.first
+    render 'one_contact.json.jb'
+  end
+
+  def all_contacts_method
+    @contacts = Contact.all #array of contacts
+    render 'all_contacts.json.jb'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/one_contact_url" => "contacts#one_contact_method"
+    get "/all_contacts_url" => "contacts#all_contacts_method"
+  end
 end
