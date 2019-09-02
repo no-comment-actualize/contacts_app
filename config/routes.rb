7387b87f001f4358f132260ddb019751a4f31454
dani <dani.zaghian@gmail.com>
2019-09-02 11:04:02 -0700

    adds restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/one_contact_url" => "contacts#one_contact_method"
-    get "/all_contacts_url" => "contacts#all_contacts_method"
+    get "/contacts" => "contacts#index"
+    get "/contacts/:id" => "contacts#show"
+    post "/contacts" => "contacts#create"
+    patch "/contacts/:id" => "contacts#update"
+    delete "/contacts/:id" => "contacts#destroy"
   end
 end
