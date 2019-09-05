37902261595c46737e0e8e9e0b7913e49e7f89c7
dani <dani.zaghian@gmail.com>
2019-09-05 14:02:59 -0700

    adds seed dump line

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-contact = Contact.new(
-  first_name: "Eddie", 
-  last_name: "Lee", 
-  email: "eddie@gmail.com", 
-  phone_number: "3333333"
-)
-contact.save
-
-contact = Contact.new(
-  first_name: "Todd", 
-  last_name: "Bisel", 
-  email: "todd@gmail.com", 
-  phone_number: "4444444"
-)
-contact.save
-
-contact = Contact.new(
-  first_name: "Jackie", 
-  last_name: "Kartchner", 
-  email: "jackie@gmail.com", 
-  phone_number: "5555555"
-)
-contact.save
+Contact.create!([
+  {first_name: "Ace", last_name: "Cat", email: "ace@gmail.com", phone_number: "1111111", middle_name: nil, bio: nil, latitude: nil, longitude: nil},
+  {first_name: "Eddie", last_name: "Lee", email: "eddie@gmail.com", phone_number: "3333333", middle_name: nil, bio: nil, latitude: nil, longitude: nil},
+  {first_name: "Todd", last_name: "Bisel", email: "todd@gmail.com", phone_number: "4444444", middle_name: nil, bio: nil, latitude: nil, longitude: nil},
+  {first_name: "Jackie", last_name: "Kartchner", email: "jackie@gmail.com", phone_number: "5555555", middle_name: nil, bio: nil, latitude: nil, longitude: nil},
+  {first_name: "Bob", last_name: "Boberson", email: "bob@gmail.com", phone_number: "555-5555", middle_name: "Robert", bio: nil, latitude: nil, longitude: nil},
+  {first_name: "Bob", last_name: "Boberson", email: "bob@gmail.com", phone_number: "555-5555", middle_name: "Robert", bio: "Loves pina coladas and getting caught in the rain.", latitude: nil, longitude: nil},
+  {first_name: "Tim", last_name: "Timmerson", email: "tim@gmail.com", phone_number: "777-7777", middle_name: "Timmy", bio: "Likes flaming hot cheetos.", latitude: nil, longitude: nil},
+  {first_name: "Dani", last_name: "Zaghian", email: "dani@gmail.com", phone_number: "888-8888", middle_name: "Unknown", bio: "Likes flaming hot cheetos.", latitude: 37.9737991677852, longitude: -122.03357661745},
+  {first_name: "Cheddar", last_name: "Cat", email: "cheesycat@gmail.com", phone_number: "2222222", middle_name: "Gozar", bio: "The destroyer.", latitude: 37.9737991677852, longitude: -122.03357661745}
+])
