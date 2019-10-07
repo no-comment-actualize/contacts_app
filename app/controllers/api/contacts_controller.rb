1190aea89b12ee3bd58d2d8613b6c6035e393764
dani <dani.zaghian@gmail.com>
2019-10-07 09:37:18 -0700

    adds cors and removes auth

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,24 +1,26 @@
 class Api::ContactsController < ApplicationController
 
   def index
-    if current_user
-      @contacts = current_user.contacts
+    # if current_user
+    #   @contacts = current_user.contacts
 
-      if params[:search]
-        @contacts = @contacts.where("first_name iLIKE ? OR last_name iLIKE ? OR middle_name iLIKE ? OR email iLIKE ? OR bio iLIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
-      end
+    #   if params[:search]
+    #     @contacts = @contacts.where("first_name iLIKE ? OR last_name iLIKE ? OR middle_name iLIKE ? OR email iLIKE ? OR bio iLIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
+    #   end
 
-      if params[:group]
-        group = Group.find_by(name: params[:group])
-        @contacts = group.contacts.where(user_id: current_user.id)
-      end
+    #   if params[:group]
+    #     group = Group.find_by(name: params[:group])
+    #     @contacts = group.contacts.where(user_id: current_user.id)
+    #   end
 
-      @contacts = @contacts.order(:id)
+    #   @contacts = @contacts.order(:id)
 
-      render 'index.json.jb'
-    else
-      render json: []
-    end
+    #   render 'index.json.jb'
+    # else
+    #   render json: []
+    # end
+    @contacts = Contact.all
+    render 'index.json.jb'
   end
 
   def create
@@ -34,7 +36,7 @@ class Api::ContactsController < ApplicationController
       bio: params[:bio],
       latitude: coordinates[0],
       longitude: coordinates[1],
-      user_id: current_user.id
+      user_id: 2
     )
     if @contact.save
       render 'show.json.jb'
@@ -51,7 +53,11 @@ class Api::ContactsController < ApplicationController
   def update
     @contact = Contact.find(params[:id])
 
-    coordinates = Geocoder.coordinates(params[:address])
+    if params[:address]
+      coordinates = Geocoder.coordinates(params[:address])
+      @contact.latitude = coordinates[0] || @contact.latitude
+      @contact.longitude = coordinates[1] || @contact.longitude
+    end
     
     @contact.first_name = params[:first_name] || @contact.first_name
     @contact.middle_name = params[:middle_name] || @contact.middle_name
@@ -59,8 +65,6 @@ class Api::ContactsController < ApplicationController
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
     @contact.bio = params[:bio] || @contact.bio
-    @contact.latitude = coordinates[0] || @contact.latitude
-    @contact.longitude = coordinates[1] || @contact.longitude
 
     if @contact.save
       render 'show.json.jb'
